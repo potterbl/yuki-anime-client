068e92b4e2b400e29391e1cc3a1260d6bfc5c2ce
potterbl <zeexample42@gmail.com>
2023-08-28 22:59:43 +0300

    mobile changes burger menu

--- src/components/WatchPlayer.jsx
+++ src/components/WatchPlayer.jsx
@@ -21,66 +21,66 @@ const WatchPlayer = (props) => {
                 </div>
 
             </div>
+            {
+                window.innerWidth <= 688 ?
+                    <button
+                        className={`menu-episode ${isMenu ? 'menu-episode-shown' : ''}`}
+                        onClick={() => setIsMenu(!isMenu)}
+                    >
+                    </button> : null
+            }
             <div className={`watch__player-right ${isMenu ? 'menu-shown' : ''}`}>
-                {   window.innerWidth <= 688 && anime.videos?
-                        anime.videos.map((seasonArr, seasonIndex) => (
-                            <>
-                                <button
-                                    className={`menu-episode ${isMenu ? 'menu-episode-shown' : ''}`}
-                                    onClick={() => setIsMenu(!isMenu)}
-                                >
-
-                                </button>
-                                <div className={`phone-menu ${isMenu ? 'menu-shown' : ''}`}>
-                                    <div key={`season-${seasonIndex}`}
-                                         className="watch__player-right-heading watch__player-right-row">
-                                        <h2 className="episode">Season {seasonIndex + 1}</h2>
+                {window.innerWidth <= 688 && anime.videos ?
+                    anime.videos.map((seasonArr, seasonIndex) => (
+                        <>
+                            <div key={`season-${seasonIndex}`}
+                                 className="watch__player-right-heading watch__player-right-row">
+                                <h2 className="episode">Season {seasonIndex + 1}</h2>
+                            </div>
+                            {
+                                seasonArr.map((episodeObj, episodeIndex) => (
+                                    <div key={`season-${seasonIndex}-episode-${episodeIndex}`}
+                                         className={`watch__player-right-row ${(seasonIndex + 1).toString() === season.toString() && episodeObj.episode.toString() === episode.toString() ? 'watch__player-right-row-active' : ''}`}
+                                         onClick={() => {
+                                             navigate(`/watch/anime/${id}/ep/${episodeIndex + 1}/s/${seasonIndex + 1}`)
+                                             window.location.reload()
+                                         }}
+                                    >
+                                        <p className="episode">Episode {episodeObj.episode}</p>
+                                        <div className="episode-description">
+                                            <p>{episodeObj.description}</p>
+                                        </div>
                                     </div>
-                                    {
-                                        seasonArr.map((episodeObj, episodeIndex) => (
-                                            <div key={`season-${seasonIndex}-episode-${episodeIndex}`}
-                                                 className={`watch__player-right-row ${(seasonIndex + 1).toString() === season.toString() && episodeObj.episode.toString() === episode.toString() ? 'watch__player-right-row-active' : ''}`}
-                                                 onClick={() => {
-                                                     navigate(`/watch/anime/${id}/ep/${episodeIndex + 1}/s/${seasonIndex + 1}`)
-                                                     window.location.reload()
-                                                 }}
-                                            >
-                                                <p className="episode">Episode {episodeObj.episode}</p>
-                                                <div className="episode-description">
-                                                    <p>{episodeObj.description}</p>
-                                                </div>
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                            </>
-                        ))
+                                ))
+                            }
+                        </>
+                    ))
                     :
                     anime.videos &&
-                        anime.videos.map((seasonArr, seasonIndex) => (
-                            <>
-                                <div key={`season-${seasonIndex}`}
-                                     className="watch__player-right-heading watch__player-right-row">
-                                    <h2 className="episode">Season {seasonIndex + 1}</h2>
-                                </div>
-                                {
-                                    seasonArr.map((episodeObj, episodeIndex) => (
-                                        <div key={`season-${seasonIndex}-episode-${episodeIndex}`}
-                                             className={`watch__player-right-row ${(seasonIndex + 1).toString() === season.toString() && episodeObj.episode.toString() === episode.toString() ? 'watch__player-right-row-active' : ''}`}
-                                            onClick={() => {
-                                                navigate(`/watch/anime/${id}/ep/${episodeIndex + 1}/s/${seasonIndex + 1}`)
-                                                window.location.reload()
-                                            }}
-                                        >
-                                            <p className="episode">Episode {episodeObj.episode}</p>
-                                            <div className="episode-description">
-                                                <p>{episodeObj.description}</p>
-                                            </div>
+                    anime.videos.map((seasonArr, seasonIndex) => (
+                        <>
+                            <div key={`season-${seasonIndex}`}
+                                 className="watch__player-right-heading watch__player-right-row">
+                                <h2 className="episode">Season {seasonIndex + 1}</h2>
+                            </div>
+                            {
+                                seasonArr.map((episodeObj, episodeIndex) => (
+                                    <div key={`season-${seasonIndex}-episode-${episodeIndex}`}
+                                         className={`watch__player-right-row ${(seasonIndex + 1).toString() === season.toString() && episodeObj.episode.toString() === episode.toString() ? 'watch__player-right-row-active' : ''}`}
+                                         onClick={() => {
+                                             navigate(`/watch/anime/${id}/ep/${episodeIndex + 1}/s/${seasonIndex + 1}`)
+                                             window.location.reload()
+                                         }}
+                                    >
+                                        <p className="episode">Episode {episodeObj.episode}</p>
+                                        <div className="episode-description">
+                                            <p>{episodeObj.description}</p>
                                         </div>
-                                    ))
-                                }
-                            </>
-                        ))
+                                    </div>
+                                ))
+                            }
+                        </>
+                    ))
                 }
             </div>
 
